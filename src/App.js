e3c8d0e499a74971ad5f045685d135f2b8ab743c
PoghosyanGevorg <gevorg1991pog@gmail.com>
2020-01-28 13:56:09 +0400

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import { Blok , Text } from './components';
 
 
-const App = () => {
+const App = (props) => {
+    useEffect((props)=>{
+        fetch('https://api.github.com/users')
+        .then(response => {
+            response.json()
+                .then((data)=>{
+                    const names = data.map(p => p.login);
+                    const avatar = data.map(p => p.avatar_url);
+                    console.log(names,avatar);
+                    
+                })
+        })
+    })
+
     return (
         <>
             <GlobalStyles />
             <h1>Hello !!!!</h1>
-            <Blok justifyContent='flex-end' width="500px" height="50px" bgColor="orange" border="5px dashed" ></Blok>
+            <Blok 
+            justifyContent='flex-end' 
+            width="500px" height="50px" 
+            bgColor="orange"
+            border="5px dashed" >privet {props.names}</Blok>
             <Text color="red" size="">fgfgfgf</Text>
         </>
     )
